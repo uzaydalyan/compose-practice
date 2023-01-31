e685f8001ac5565429c3753f0a5af812833fbcf6
uzaydalyan <uzaydalyan@gmail.com>
2023-01-31 15:44:53 +0300

    textField button and snackbar are done

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,19 +3,63 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposepracticeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            var textState by remember {
+                mutableStateOf("")
+            }
+
+            val scope = rememberCoroutineScope()
+
+            Scaffold(
+                scaffoldState = scaffoldState,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textState,
+                        label = {
+                            Text("Enter your name")
+                        },
+                        onValueChange = {
+                            textState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            scope.launch{
+                                scaffoldState.snackbarHostState.showSnackbar(message = "Hello, $textState")
+                            }
+                        }
+                    ) {
+                        Text("Submit")
+                    }
+                }
+            }
 
         }
     }
